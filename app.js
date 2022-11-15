d0a0c85a866873f5b9859bb2032135a8b6014d28
Valerii Stolbov <stolbov.v@outlook.com>
2022-11-16 03:01:57 +0500

    udate server

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const config = require('./config.json');
-
-const _host = config.host || '127.0.0.1';
-const _port = config.port || 5000;
 
 const app = express();
+const port = 5000;
+const host = '127.0.0.1';
 
 app.use('/api/fm', require('./routes/fm.routes'));
 
-app.listen(_port, _host, () => console.log(`Server listen http://${_host}:${_port}...`));
+app.listen(port, host, () => console.log(`Server listen http://${host}:${port}...`));
--- routes/fm.routes.js
+++ routes/fm.routes.js
@@ -1,61 +1,98 @@
 const axios = require('axios');
 const router = require('express').Router();
-const config = require('../config.json');
 
-const api = config.api || 'https://web.fort-monitor.ru/api/integration/v1';
-
-const cors = {
-  ['Access-Control-Allow-Credentials']: true,
-  ['Access-Control-Allow-Headers']: 'Content-Type',
-  ['Access-Control-Allow-Origin']: 'http://localhost:3000',
-};
-
-router.get('/connect', (request, response) => {
-  try {
-    const { login, password, lang, timezone } = request.query;
-    axios({
-      url: `${api}/connect?login=${login}&password=${password}&lang=${lang}&timezone=${timezone}`,
-      method: 'get',
-      headers: {
-        ['Content-type']: 'application/json; charset=utf-8',
-      },
-    })
-      .then((res) => {
-        response
-          .header({ ...cors })
-          .status(res.status)
-          .send({ data: res.data, sid: res.headers.sessionid });
-      })
-      .catch((err) => {
-        response.status(500).send({ message: err });
-      });
-  } catch {
-    response.status(500).send({ message: 'Internal Server Error' });
-  }
+const instans = axios.create({
+  baseURL: 'https://web.fort-monitor.ru/api/integration/v1',
+  headers: {
+    'Content-type': 'application/json; charset=utf-8',
+  },
 });
 
-router.get('/disconect', (request, response) => {
-  try {
-    axios({
-      url: `${api}/disconnect`,
-      method: 'get',
-      headers: {
-        ['Content-type']: 'application/json; charset=utf-8',
-        ['SessionId']: request.headers['user-sid'],
+function handleErrorResponse(response) {
+  return (err) => {
+    if (err.res) {
+      response.status(err.res.status).send(err.res.data).end();
+    } else if (err.req) {
+      response.status(504).send(err.req).end();
+    } else {
+      response.status(500).send(err.message).end();
+    }
+  };
+}
+
+router.get('/connect', (request, response) => {
+  instans
+    .get('/connect', {
+      params: {
+        lang: request.query.lang,
+        login: request.query.login,
+        password: request.query.password,
+        timezone: request.query.timezone,
       },
     })
-      .then((res) =>
-        response
-          .header({ ...cors })
-          .status(res.statusCode)
-          .send({ data: res.data })
-      )
-      .catch((err) => {
-        response.status(500).send({ message: err });
-      });
-  } catch {
-    response.status(500).send({ message: 'Internal Server Error' });
-  }
+    .then((res) => {
+      response
+        .status(res.status)
+        .set({ 'access-control-allow-origin': '*' })
+        .send({ message: res.data, sid: res.headers.sessionid })
+        .end();
+    })
+    .catch(handleErrorResponse(response));
+});
+
+router.get('/disconnect', (request, response) => {
+  instans
+    .get('/disconnect', {
+      headers: {
+        sessionid: request.headers['user-sid'],
+      },
+    })
+    .then((res) => {
+      response.status(res.status).set({ 'Access-Control-Allow-Origin': '*' }).send({ message: res.data }).end();
+    })
+    .catch(handleErrorResponse(response));
+});
+
+router.get('/getcompanieslist', (request, response) => {
+  instans
+    .get('/getcompanieslist', {
+      headers: {
+        sessionid: request.headers['user-sid'],
+      },
+    })
+    .then((res) => {
+      response.status(res.status).set({ 'Access-Control-Allow-Origin': '*' }).send(res.data).end();
+    })
+    .catch(handleErrorResponse(response));
+});
+
+router.get('/getobjectgroupslist', (request, response) => {
+  instans
+    .get('/getobjectgroupslist', {
+      headers: {
+        sessionid: request.headers['user-sid'],
+      },
+    })
+    .then((res) => {
+      response.status(res.status).set({ 'Access-Control-Allow-Origin': '*' }).send(res.data).end();
+    })
+    .catch(handleErrorResponse(response));
+});
+
+router.get('getobjectslist', (request, response) => {
+  instans
+    .get('getobjectslist', {
+      headers: {
+        sessionid: request.headers['user-sid'],
+      },
+      params: {
+        companyId: request.query,
+      },
+    })
+    .then((res) => {
+      response.status(res.status).set({ 'Access-Control-Allow-Origin': '*' }).send(res.data).end();
+    })
+    .catch(handleErrorResponse(response));
 });
 
 module.exports = router;
